f3d367829c99b6c2a0b421d3954e60780867a194
nrenner <nrenner0211@gmail.com>
2022-06-05 16:11:15 -0400

    css updated, localstorage working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,52 +2,13 @@
 var currentTime = moment().format('dddd, MMMM Do YYYY, h:mm a');
 $("#currentDay").html(currentTime);
 
-
 $(document).ready(function() {
-    function timeAlert() {
-        //get hours
-        var timeNow = moment().hour();
+    //save event listener
+    $(".saveBtn").on('click', function() {
+        var text = $(this).siblings(".input").val();
+        var time = $(this).parent().attr("id");
 
-        $("time-block").each(function() {
-            var hourlyTime = parseInt($(this).attr("id").split("hour")[1]);
-
-            //add classes per time
-            if (hourlyTime < currentTime) {
-                $(this).addClass("past");
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-            }
-            if (hourlyTime === currentTime) {
-                $(this).addClass("present");
-                $(this).removeClass("future");
-                $(this).removeClass("past");
-            } else {
-                $(this).addClass("future");
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-            }
+        //save to local storage
+        localStorage.setItem(time, text);
     })
-}})
-
-// function loadQuestion() {
-//     //question appears onscreen
-//     questionEl.textContent = quizQuestions[questionIndex].question;
-//     //clears old answer choices
-//     answerEl.innerHTML = ""; 
-
-//     var questionChoices = quizQuestions[questionIndex].choices
-//     var sortedChoices = questionChoices.sort().reverse();
-//     console.log(sortedChoices);
-//     for (var i in questionChoices) {
-//         var item = questionChoices[i];
-//         var answerBtn = document.createElement('button');
-//         answerBtn.textContent = item;
-//         answerBtn.classList.add("btn");
-//         answerBtn.addEventListener("click", function () {
-//             //when answer clicked, check answer
-//             checkAnswer(this.textContent);
-//         });
-//         //add buttons to screen
-//         answerEl.appendChild(answerBtn);
-//     }
-// };
+})
