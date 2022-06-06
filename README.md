74a9965ac3c0a28b5295f460dbf6dc90d32ba286
nrenner <nrenner0211@gmail.com>
2022-06-05 21:27:03 -0400

    readme updated

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # planner-app
 
-Challenge 5
+## About
+
+This project's purpose was to learn about third-party libraries, such as Bootstrap and jQuery. I also chose to use Moment.js. I overthought it and struggled at first, naturally. But the documentation was straightforward and useful for all three libraries. 10/10 would use again!
 
 ## Starter Code
 
 [https://github.com/coding-boot-camp/super-disco]
+
+## Screen Capture
+
+![A .gif of the working app] (assets/images/screen-capture.gif)
+
+## Thanks For Reading
+
+♡ Niki Renner
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,54 @@ $(document).ready(function() {
         //save to local storage
         localStorage.setItem(time, text);
     })
+
+    function timeOfDay() {
+        
+        //get hours
+        var timeNow = moment().hour();
+
+        // timeblocks
+        $(".time-block").each(function() {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            //add classes per time
+            if (blockTime < timeNow) {
+
+                //remove old classes
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+
+                //add new class
+                $(this).addClass("past");
+                
+            } else if (blockTime === timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    $("#hour8 .input").val(localStorage.getItem("hour8"));
+    $("#hour9 .input").val(localStorage.getItem("hour9"));
+    $("#hour10 .input").val(localStorage.getItem("hour10"));
+    $("#hour11 .input").val(localStorage.getItem("hour11"));
+    $("#hour12 .input").val(localStorage.getItem("hour12"));
+    $("#hour13 .input").val(localStorage.getItem("hour13"));
+    $("#hour14 .input").val(localStorage.getItem("hour14"));
+    $("#hour15 .input").val(localStorage.getItem("hour15"));
+    $("#hour16 .input").val(localStorage.getItem("hour16"));
+    $("#hour17 .input").val(localStorage.getItem("hour17"));
+    $("#hour18 .input").val(localStorage.getItem("hour17"));
+    $("#hour19 .input").val(localStorage.getItem("hour17"));
+    $("#hour20 .input").val(localStorage.getItem("hour17"));
+
+
+    timeOfDay();
 })
+
+
